6b5ac99a855298854225b18c647894758990f9bd
MrMetrail <zenroeski@aol.com>
2019-06-24 13:36:09 -0600

    app and database setup

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mustard, Pickle & Cheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Avacado & Bacon', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chipolte Rattlesnake', false);
